3a297fdf4dc7bb8ce8c65d81b114c12cfc69cba9
gabrielotsuka <gabrielotsuka@gmail.com>
2021-03-10 14:28:05 -0300

    default algorithm

--- src/com/afd/Main.java
+++ src/com/afd/Main.java
@@ -1,9 +1,29 @@
 package com.afd;
 
-import com.afd.interfaces.Windows;
+import com.afd.data.Automaton;
+import com.afd.data.Rule;
 
 public class Main {
-    public static void main(String[] args) {
-        new Windows();
+    public static void main(String[] args) throws Exception {
+        Automaton M = Automaton.buildDefaultAutomaton();
+        String sequence = "101010101000";
+
+        String currentState = M.getInitialState();
+        for (int currentPosition = 0; currentPosition < sequence.length(); currentPosition++) {
+            for (Rule rule : M.getRules()) {
+                if (rule.getSourceState().equals(currentState) && rule.getSymbol() == sequence.charAt(currentPosition)) {
+                    currentState = rule.getTargetState();
+                    break;
+                }
+            }
+        }
+
+        if (M.getFinalStates().contains(currentState)) {
+            System.out.println("DEU BÃO");
+        } else {
+            System.out.println("DEU RUIM");
+        }
     }
+
+
 }
--- src/com/afd/data/Automaton.java
+++ src/com/afd/data/Automaton.java
@@ -1,5 +1,6 @@
 package com.afd.data;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Automaton {
@@ -8,4 +9,59 @@ public class Automaton {
     private Set<Rule> rules;
     private String initialState;
     private Set<String> finalStates;
+
+    public Automaton(Set<String> states, Set<String> alphabet, Set<Rule> rules, String initialState, Set<String> finalStates) {
+        this.states = states;
+        this.alphabet = alphabet;
+        this.rules = rules;
+        this.initialState = initialState;
+        this.finalStates = finalStates;
+    }
+
+    public static Automaton buildDefaultAutomaton() {
+        Set<String> states = new HashSet<>();
+        states.add("q0");
+        states.add("q1");
+        states.add("q2");
+
+        Set<String> alphabet = new HashSet<>();
+        alphabet.add("0");
+        alphabet.add("1");
+
+        Set<Rule> rules = new HashSet<>();
+        Rule rule1 = new Rule("q0", '0', "q1");
+        Rule rule2 = new Rule("q0", '1', "q0");
+        Rule rule3 = new Rule("q1", '0', "q1");
+        Rule rule4 = new Rule("q1", '1', "q2");
+        Rule rule5 = new Rule("q2", '0', "q2");
+        Rule rule6 = new Rule("q2", '1', "q2");
+        rules.add(rule1);rules.add(rule2);rules.add(rule3);rules.add(rule4);rules.add(rule5);rules.add(rule6);
+
+        String initialState = "q0";
+
+        Set<String> finalStates = new HashSet<>();
+        finalStates.add("q2");
+
+        return new Automaton(states, alphabet, rules, initialState, finalStates);
+    }
+
+    public Set<String> getStates() {
+        return states;
+    }
+
+    public Set<String> getAlphabet() {
+        return alphabet;
+    }
+
+    public Set<Rule> getRules() {
+        return rules;
+    }
+
+    public String getInitialState() {
+        return initialState;
+    }
+
+    public Set<String> getFinalStates() {
+        return finalStates;
+    }
 }
