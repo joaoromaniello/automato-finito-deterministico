3fa4d772bd35c0e90dd4740050513fc08277569a
gabrielotsuka <gabrielotsuka@gmail.com>
2021-03-10 19:24:35 -0300

    invalid symbol treatment

--- src/com/afd/Main.java
+++ src/com/afd/Main.java
@@ -6,15 +6,21 @@ import com.afd.data.Rule;
 public class Main {
     public static void main(String[] args) throws Exception {
         Automaton M = Automaton.buildDefaultAutomaton();
-        String sequence = "101010101000";
+        String sequence = "01a";
 
         String currentState = M.getInitialState();
+        boolean isValidSymbol = false;
         for (int currentPosition = 0; currentPosition < sequence.length(); currentPosition++) {
+            isValidSymbol = false;
             for (Rule rule : M.getRules()) {
                 if (rule.getSourceState().equals(currentState) && rule.getSymbol() == sequence.charAt(currentPosition)) {
                     currentState = rule.getTargetState();
+                    isValidSymbol = true;
                     break;
                 }
+            }
+            if (!isValidSymbol) {
+                throw new Exception("Símbolo não está no alfabeto");
             }
         }
 
