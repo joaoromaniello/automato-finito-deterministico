683b41fb7e480f0d76b9f2355aae4ae1b9d1c55f
Joao Nardini <jaromaniello@gmail.com>
2021-03-18 12:40:27 -0300

    Tentando criar Interface do resultado

--- src/com/afd/interfaces/MainWindows.java
+++ src/com/afd/interfaces/MainWindows.java
@@ -3,6 +3,7 @@ package com.afd.interfaces;
 
 public class MainWindows {
     public static void main(String[] args) throws Exception {
-        new StateWindows();
+        new resultInterface();
+        //new StateWindows();
     }
 }
