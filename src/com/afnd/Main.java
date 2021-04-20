351c4a205e260b108dc77af1c20fa871cd960cf1
joaoromaniello <jaromaniello@gmail.com>
2021-04-19 23:19:22 -0300

    ....

--- src/com/afnd/Main.java
+++ src/com/afnd/Main.java
@@ -1,12 +1,11 @@
 package com.afnd;
 
-import com.afnd.view.AFNDview;
 import com.afnd.view.InitialView;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // new AFNDview();
+        new InitialView();
     }
 }
